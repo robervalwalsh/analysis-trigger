d70640a66fce278055dd4ce50bd464b697a45348
Roberval Walsh <roberval.walsh@gmail.com>
2017-04-25 13:58:44 +0200

    improvements

--- macros/HLTRatesData.C
+++ macros/HLTRatesData.C
@@ -1,12 +1,23 @@
-void HLTRatesData()
+void HLTRatesData(const std::string & trigger, const std::string & file)
 {
+   // All hadronic
+//   trigger = "HLT_2CaloJets100_2CaloBTagCSV084_2PFJets100_psw";
+//   trigger = "HLT_2CaloJets100_2CaloBTagCSV092_2PFJets100_psw";
+//   trigger = "HLT_2CaloJets100_2CaloBTagCSV092_2PFJets100_Eta2p1_psw";
+//   trigger = "HLT_2CaloJets100_2CaloBTagCSV092_2PFJets100_Eta2p1_dEta1p5_psw";
+   
+//   trigger = "HLT_2CaloJets30_Muon12_2CaloBTagCSV084_2PFJets40_psw";
+//   trigger = "HLT_2CaloJets30_Muon12_2CaloBTagCSV092_2PFJets40_psw";
+//   trigger = "HLT_2CaloJets30_Muon12_2CaloBTagCSV092_2PFJets50e40_Eta2p1_psw";
+//   trigger = "HLT_2CaloJets30_Muon12_2CaloBTagCSV092_2PFJets50e40_Eta2p1_dEta1p5_psw";
+   
+   
+
    double nbx = 2600;
-//   TFile * inFile = new TFile("../test/80x/BTagMu20/mssmhbb_triggers_data_BTagMu20.root","old");
-   TFile * inFile = new TFile("../test/80x/BTagCSV/mssmhbb_triggers_data_BTagCSV.root","old");
-//   TFile * inFile = new TFile("../test/80x/HighPU/mssmhbb_triggers_data_zerobiasbt.root","old");
+   TFile * inFile = new TFile(file.c_str(),"old");
+//   TFile * inFile = new TFile("../test/80x/BTagCSV/mssmhbb_triggers_data_BTagCSV.root","old");
    TH1F * hLumis   = (TH1F*) inFile -> Get("Lumis");
-//   TH1F * hpath = (TH1F*) inFile -> Get("h_nHLT_2CaloJets30_Muon12_2CaloBTagCSV092_2PFJets50e40_Eta2p1_dEta1p5_psw");
-   TH1F * hpath = (TH1F*) inFile -> Get("h_nHLT_2CaloJets100_2CaloBTagCSV092_2PFJets100_Eta2p1_dEta1p5_psw");
+   TH1F * hpath = (TH1F*) inFile -> Get(Form("h_n%s",trigger.c_str()));
    
    double pileup[100];
    double pileupErr[100];
@@ -18,7 +29,7 @@ void HLTRatesData()
    for ( int i = 0; i < hLumis->GetNbinsX(); ++i )
    {
       double pu = hLumis->GetBinCenter(i+1);
-      if ( pu < 14 ) continue;
+      if ( pu < 14 || pu == 32.5 || pu > 44 ) continue;
       pileup[n] = pu;
       pileupErr[n] = 0;
       double nlumi = hLumis->GetBinContent(i+1);
@@ -34,9 +45,15 @@ void HLTRatesData()
    }
    
    TGraphErrors * gRate = new TGraphErrors(n,pileup,rate,pileupErr,rateErr);
+   gRate -> SetName("rate_total");
    gRate -> SetMarkerStyle(20);
    gRate -> Draw("AP");
    
+   TFile * out = new TFile(Form("data_%d_bx_%s.root",int(nbx),trigger.c_str()),"recreate");
+   gRate -> Write();
+   
+   out -> Close();
 
+   
 }
 
