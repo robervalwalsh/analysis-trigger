66acaf0c4b0f425370ba231e6e63ff811574c25a
Roberval Walsh <roberval.walsh@gmail.com>
2017-06-06 14:40:28 +0200

    JetHT

---
+++ test/80x/JetHT/efficiency/efficiency_1x.C
@@ -0,0 +1,37 @@
+void efficiency_1x()
+{
+   TFile*  f = new TFile("mssmhbb_triggers_data_JetHT.root","old");
+   TH1F * h1 = (TH1F*) f -> Get("muon12");
+   TH1F * h2 = (TH1F*) f -> Get("muon12Trg");
+   
+   h1 -> Rebin(5);
+   h2 -> Rebin(5);
+   
+   TGraphAsymmErrors * g1 = new TGraphAsymmErrors(h2,h1,"cl=0.683 b(1,1) mode");
+   
+   g1 -> SetMarkerStyle(20);
+   g1 -> SetMarkerColor(kBlack);
+   g1 -> SetLineColor(kBlack);
+   
+   TCanvas * c1 = new TCanvas("c1","", 700,600);
+
+   TMultiGraph * mg = new TMultiGraph();
+   mg -> Add(g1);
+   
+   mg -> Draw("AP");
+   
+   TLegend * leg = new TLegend(0.45,0.15,0.85,0.3);
+   leg->SetHeader("online csv > 0.92");
+   leg->AddEntry(g1,"jet pT > 100 GeV","pl");
+   
+//   leg -> Draw();
+   mg -> GetYaxis() -> SetTitle("efficiency");
+   mg -> GetXaxis() -> SetTitle("jet  pt(GeV)");
+   mg -> GetXaxis() -> SetTitleOffset(1.2); 
+   mg -> GetYaxis() -> SetTitleOffset(1.3); 
+   
+   gPad->Modified();
+   
+   c1 -> SaveAs("muon12_eff_data80x_jetht.png");
+   
+}
