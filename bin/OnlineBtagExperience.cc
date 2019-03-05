6ade97f6c74ead4442aa2a4d924590ad1caaa235
Roberval Walsh <roberval.walsh@gmail.com>
2019-03-05 18:46:01 +0100

    using the new OnlineBtagAnalyser class

--- bin/OnlineBtagExperience.cc
+++ bin/OnlineBtagExperience.cc
@@ -10,11 +10,13 @@
 #include "TChain.h"
 #include "TH1.h" 
 
-#include "Analysis/Tools/interface/Analyser.h"
+//#include "Analysis/Tools/interface/Analyser.h"
+#include "Analysis/Trigger/interface/OnlineBtagAnalyser.h"
 
 using namespace std;
 using namespace analysis;
-using namespace analysis::tools;
+//using namespace analysis::tools;
+using namespace analysis::trigger;
 
 using TH1s = std::map<std::string, TH1F*>;
 using TH2s = std::map<std::string, TH2F*>;
@@ -25,7 +27,7 @@ int main(int argc, char ** argv)
 {
    TH1::SetDefaultSumw2();  // proper treatment of errors when scaling histograms
    
-   Analyser onlinebtag(argc,argv);
+   OnlineBtagAnalyser onlinebtag(argc,argv);
    
    onlinebtag.jetHistograms(2,"tag");
    onlinebtag.jetHistograms(2,"probe");
@@ -89,6 +91,59 @@ int main(int argc, char ** argv)
       // fill probe histograms after selection
          onlinebtag.fillJetHistograms("probe_match");
       }
+      
+      
+      if ( onlinebtag.config()->workflow() == 2 )  // ========== DATA and MC with data-like sequence - SEMILEPTONIC ========
+      {
+      
+      // trigger selection
+         if ( ! onlinebtag.selectionHLT()                 )   continue;
+         if ( ! onlinebtag.selectionL1 ()                 )   continue;  // to be used in case of "OR" of seeds
+      
+      // muon identification selection
+         if ( ! mssmhbb.selectionMuonId()                 )   continue;
+         if ( ! mssmhbb.selectionNMuons()                 )   continue;
+         if ( ! mssmhbb.selectionMuons()                  )   continue;
+         if ( ! mssmhbb.onlineMuonMatching()              )   continue; // FIXME there: should change the selectedMuons ????
+            
+      // jet identification selection
+         if ( ! onlinebtag.selectionJetId()               )   continue;
+         if ( ! onlinebtag.selectionJetPileupId()         )   continue;
+         if ( ! onlinebtag.selectionNJets()               )   continue;
+         
+      // delta Phi
+         if ( ! onlinebtag.selectionJetDphi(1,2)          )   continue;
+      // pT imbalance
+         if ( ! onlinebtag.selectionJetPtImbalance(1,2)   )   continue;
+         
+      // Muon jet association
+         if ( ! onlinebtag.muonJet()                      )   continue;
+         
+      // tag jet selection
+         if ( ! onlinebtag.selectionJet(2)                )   continue;
+         if ( ! onlinebtag.selectionJetQGlikelihood(2)    )   continue;
+         if ( ! onlinebtag.onlineJetMatching(2)           )   continue;
+         if ( ! onlinebtag.selectionBJet(2)               )   continue;
+         if ( ! onlinebtag.onlineBJetMatching(2)          )   continue;
+         onlinebtag.fillJetHistograms("tag");
+         
+      // probe jet selection
+      // qg likelihood
+         if ( ! onlinebtag.selectionJet(1)                )   continue;
+         if ( ! onlinebtag.selectionJetQGlikelihood(1)    )   continue;
+         if ( ! onlinebtag.onlineJetMatching(1)           )   continue;
+         if ( ! onlinebtag.selectionBJet(1)               )   continue;
+         
+      // fill tag histograms after selection
+         onlinebtag.fillJetHistograms("probe");
+         
+      // PROBE jet match to online
+         if ( ! onlinebtag.onlineBJetMatching(1)          )   continue;
+         
+      // fill probe histograms after selection
+         onlinebtag.fillJetHistograms("probe_match");
+      }
+      
    }
    
 } //end main
